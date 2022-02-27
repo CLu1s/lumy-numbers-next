46f0f73c04cd4b2cbe37a092ed814c01d29cbaab
Luis Lopez <luis.luis@icloud.com>
2022-02-26 23:13:19 -0600

    hotfix pwa 3

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  future: { webpack5: true },
   pwa: {
-    dest: 'public',
+    dest: "public",
     runtimeCaching,
     disable: process.env.NODE_ENV === "development",
   },
