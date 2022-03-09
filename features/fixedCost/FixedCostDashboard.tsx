69cc641af42e806e3e15bd2a85335ffc7009e811
Luis Lopez <luis.luis@icloud.com>
2022-03-09 17:57:31 -0600

    add needed amount and refactor fixed cost

--- features/fixedCost/FixedCostDashboard.tsx
+++ features/fixedCost/FixedCostDashboard.tsx
@@ -1,19 +1,28 @@
 import { useMemo, useEffect, useState, useCallback } from "react";
 import Link from "next/link";
-import { Stack, Wrap, WrapItem, Link as ChakraLink } from "@chakra-ui/react";
+import {
+  Stack,
+  Wrap,
+  WrapItem,
+  Link as ChakraLink,
+  chakra,
+  Text,
+} from "@chakra-ui/react";
 import { useSelector, useDispatch } from "react-redux";
 import Screen from "../../components/Screen";
 import ItemsList from "./ItemsList";
-import { getPendingItems, getCategoryID } from "./selector";
+import { getPendingItems, getPendingAmount, getCategoryID } from "./selector";
 import { getBucketID } from "../bucket/selector";
 import { updateFixedCost } from "./fixedCostSlice";
 import { addNewTransaction } from "../wallet/walletSlice";
+import { money } from "../../utils";
 
 const FixedCostDashboard = () => {
   const items = useSelector(getPendingItems);
   const dispatch = useDispatch();
   const bucketID = useSelector(getBucketID);
   const categoryID = useSelector(getCategoryID);
+  const amount = useSelector(getPendingAmount);
 
   const managePaid = (data: any) => {
     const { createdAt, updatedAt, ...input } = data;
@@ -30,10 +39,16 @@ const FixedCostDashboard = () => {
   };
   return (
     <Screen title="Gastos Pendientes">
-      <ItemsList items={items} managePaid={managePaid} />
-      <Link href="/costos-fijos" passHref>
-        <ChakraLink color="teal.500">Ver todo</ChakraLink>
-      </Link>
+      <Stack spacing={4}>
+        <Text  fontSize='md'>
+          Cantidad necesaria para pagar los gastos pendientes:
+          <chakra.span fontWeight="bold"  fontSize='xl'> {money(amount)}</chakra.span>
+        </Text>
+        <ItemsList items={items} managePaid={managePaid} />
+        <Link href="/costos-fijos" passHref>
+          <ChakraLink color="teal.500">Ver Todos los Gastos</ChakraLink>
+        </Link>
+      </Stack>
     </Screen>
   );
 };
--- features/fixedCost/selector.ts
+++ features/fixedCost/selector.ts
@@ -1,20 +1,68 @@
 import { createSelector } from "reselect";
-import { FixedCostState, Bucket } from "../../types";
+import { FixedCostState, Bucket, WalletState } from "../../types";
 import { RootState } from "../../store/reducers";
 import _orderBy from "lodash/orderBy";
 
 const FixedCostSelector = (state: RootState): FixedCostState => state.fixedCost;
 const BucketSelector = (state: RootState): Bucket => state.bucket.bucket;
+const walletSelector = (state: RootState): WalletState => state.wallet;
+
+const orderItems = (
+  items: FixedCostState["items"],
+  transactions: WalletState["transactions"],
+  fixedCostCategoryID: string
+) =>
+  items.map((item: FixedCostState["items"][0]) => {
+    const transaction = transactions.findIndex(
+      (transaction: WalletState["transactions"][0]) =>
+        transaction.description === item.description &&
+        transaction.categoryID === fixedCostCategoryID
+    );
+    return {
+      ...item,
+      status: transaction > -1 ? "paid" : "unpaid",
+    };
+  });
 
 export const getItems = createSelector(
-  [FixedCostSelector],
-  (state: FixedCostState): FixedCostState["items"] =>
-    _orderBy(state.items, ["status"], ["desc"])
+  [FixedCostSelector, walletSelector, BucketSelector],
+  (
+    state: FixedCostState,
+    walletState: WalletState,
+    bucketState: Bucket
+  ): FixedCostState["items"] => {
+    const transactions = walletState.transactions;
+    const items = orderItems(
+      state.items,
+      transactions,
+      bucketState.fixedCostCategoryID
+    );
+    return _orderBy(items, ["status"], ["desc"]);
+  }
 );
 export const getPendingItems = createSelector(
+  [FixedCostSelector, walletSelector, BucketSelector],
+  (
+    state: FixedCostState,
+    walletState: WalletState,
+    bucketState: Bucket
+  ): FixedCostState["items"] => {
+    const transactions = walletState.transactions;
+    const items = orderItems(
+      state.items,
+      transactions,
+      bucketState.fixedCostCategoryID
+    );
+    return items.filter((item) => item.status !== "paid");
+  }
+);
+
+export const getPendingAmount = createSelector(
   [FixedCostSelector],
-  (state: FixedCostState): FixedCostState["items"] =>
-    state.items.filter((item) => item.status !== "paid")
+  (state: FixedCostState): number =>
+    state.items
+      .filter((i) => i.status !== "paid")
+      .reduce((acc, item) => acc + item.amount, 0)
 );
 
 export const getStatus = createSelector(
