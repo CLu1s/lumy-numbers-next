1b7705799339d8565b121d0f51b23fa87aeb1c8b
Luis Lopez <luis.luis@icloud.com>
2022-05-13 17:04:03 -0500

    add transactions filters

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -9,6 +9,7 @@ import {
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
+  VStack,
 } from "@chakra-ui/react";
 import { Category } from "../types";
 
@@ -19,36 +20,49 @@ type Props = {
 };
 
 const Filters = ({ categories, filter, setFilter }: Props) => {
+  const checkboxGroup = (
+    <CheckboxGroup
+      colorScheme="blue"
+      defaultValue={filter}
+      onChange={(value: string[]) => {
+        setFilter(value);
+      }}
+    >
+      <Stack spacing={[1, 5]} direction={["column", "row"]}>
+        {categories.map((category) => (
+          <Checkbox key={category.id} value={category.id}>
+            {category.name}
+          </Checkbox>
+        ))}
+      </Stack>
+    </CheckboxGroup>
+  );
   return (
-    <Accordion>
-      <AccordionItem>
-        <h2>
-          <AccordionButton>
-            <Box flex="1" textAlign="left">
-              Filtrar por Categoría
-            </Box>
-            <AccordionIcon />
-          </AccordionButton>
-        </h2>
-        <AccordionPanel pb={4}>
-          <CheckboxGroup
-            colorScheme="blue"
-            defaultValue={filter}
-            onChange={(value: string[]) => {
-              setFilter(value);
-            }}
-          >
-            <Stack spacing={[1, 5]} direction={["column", "row"]}>
-              {categories.map((category) => (
-                <Checkbox key={category.id} value={category.id}>
-                  {category.name}
-                </Checkbox>
-              ))}
-            </Stack>
-          </CheckboxGroup>
-        </AccordionPanel>
-      </AccordionItem>
-    </Accordion>
+    <Box>
+      <VStack align="flex-start">
+        <Box display={{ base: "none", lg: "block" }} flex="1" textAlign="left">
+          Filtrar por Categoría
+        </Box>
+        <Box display={{ base: "none", lg: "block" }} width="full">
+          {checkboxGroup}
+        </Box>
+      </VStack>
+      <Box display={{ base: "block", lg: "none" }}>
+        <Accordion>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  Filtrar por Categoría
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>{checkboxGroup}</AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </Box>
+    </Box>
   );
 };
 export default Filters;
