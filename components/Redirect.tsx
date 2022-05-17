22fd030d835a6dd0123a112aea3fe777cf55837b
Luis Lopez <luis.luis@icloud.com>
2022-05-16 19:08:32 -0500

    add loading screen

--- components/Redirect.tsx
+++ components/Redirect.tsx
@@ -1,16 +1,30 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import { Heading, Text, VStack } from "@chakra-ui/react";
+import Loading from "./Loading";
+import logo from "../public/logo.png";
 
 const Redirect = ({ href }) => {
   const router = useRouter();
   useEffect(() => {
     router.push(href);
-  }, []);
+  }, [href, router]);
   return (
-    <div>
-      <h1>{router.query.name}</h1>
-      <p>Redirigiendo...</p>
-    </div>
+    <VStack marginTop="50%" spacing="8">
+      <Image
+        src={logo}
+        alt="Logo"
+        width={192}
+        height={192}
+        placeholder="blur"
+      />
+      <Heading>Luminus</Heading>
+      <Text>Gasto Consciente</Text>
+
+      <Loading />
+      <Text>Cargando...</Text>
+    </VStack>
   );
 };
 
